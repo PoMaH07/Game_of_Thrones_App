c8159990e702fef62e9208d2a16504e60f953396
Roman <gio92641@gmail.com>
2021-08-07 19:07:57 +0300

    finish get fetch requiest

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-spin-s042e3vsrfi"><div className="ldio-0kxzsii0sxym">
+            <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
